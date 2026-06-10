ab15fa7fab457c5d502ba4b3e4b16a1ea3912528
ANSHUMAN YADAV <145282790+Student-Anshuman@users.noreply.github.com>
2026-06-10 22:25:38 +0530

    fixing api

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,6 +26,9 @@ app.use(clerkMiddleware()); // this adds auth field to request object: req.auth(
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
+app.get('/', (req, res) => {
+  res.send("Backend is running!");
+});
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
