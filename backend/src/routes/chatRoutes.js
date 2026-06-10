1f0066eb57e8f94b9eee78a8f0e6e9fa0fc8b779
ANSHUMAN YADAV <145282790+Student-Anshuman@users.noreply.github.com>
2026-06-10 11:53:46 +0530

    fixing protect routes

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -4,6 +4,6 @@ import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/token", protectRoute, getStreamToken);
+router.get("/token", getStreamToken);
 
 export default router;
