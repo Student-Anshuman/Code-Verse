d61fb86d911fae9253b2893b0ef1df1092345125
ANSHUMAN YADAV <145282790+Student-Anshuman@users.noreply.github.com>
2026-06-10 19:56:53 +0530

    protect routes fixing

--- backend/src/routes/sessionRoute.js
+++ backend/src/routes/sessionRoute.js
@@ -16,7 +16,7 @@ router.get("/test", (req, res) => {
 
 router.post("/", protectRoute, createSession);
 router.get("/active", getActiveSessions);
-router.get("/my-recent", protectRoute, getMyRecentSessions);
+router.get("/my-recent", getMyRecentSessions);
 
 router.get("/:id", getSessionById);
 router.post("/:id/join", protectRoute, joinSession);
